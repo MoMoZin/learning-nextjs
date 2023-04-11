021a425672eba6c0d2e396c84cbc4db068c2282e
kmomozin <khinmomozin@gmail.com>
2023-04-11 11:59:32 -0600

    dynamic page with data fetching using getstaticprops

--- first-next-app/pages/index.js
+++ first-next-app/pages/index.js
@@ -2,28 +2,26 @@ import Link from "next/link";
 import Component from "../components/Component";
 import Head from "next/head";
 
-const index = () => {
+const index = ({ data }) => {
   return (
     <div>
       <Head>
         <title>Home | Learn Next.js</title>
         <meta name="keywords" content="Next.js, javascript, react" />
       </Head>
-      <Link href="/about">
-        <span className="about">Go to about page</span>
-      </Link>
-      <Component />
-      <style jsx>
-        {
-          `
-          .about{
-            color:brown;
-          }
-          `
-        }
-      </style>
+      {
+        data.map(todo => <h1>{todo.title}</h1>)
+      }
     </div>
   );
 };
 
 export default index;
+
+export async function getStaticProps() {
+  const req = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await req.json();
+  return {
+    props: { data }
+  };
+}
