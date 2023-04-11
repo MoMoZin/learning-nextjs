70a445fe589dc69e3bd09b68be5af2370d816a52
kmomozin <khinmomozin@gmail.com>
2023-04-11 11:41:20 -0600

    add next head component for title and SEO

--- first-next-app/pages/index.js
+++ first-next-app/pages/index.js
@@ -1,9 +1,14 @@
 import Link from "next/link";
 import Component from "../components/Component";
+import Head from "next/head";
 
 const index = () => {
   return (
     <div>
+      <Head>
+        <title>Home | Learn Next.js</title>
+        <meta name="keywords" content="Next.js, javascript, react" />
+      </Head>
       <Link href="/about">
         <span className="about">Go to about page</span>
       </Link>
