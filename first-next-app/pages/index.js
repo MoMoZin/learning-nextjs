fe6eba0a5bbdd364422c8899e9afa4829d63d85f
kmomozin <khinmomozin@gmail.com>
2023-04-11 11:23:21 -0600

    set up component layout

--- first-next-app/pages/index.js
+++ first-next-app/pages/index.js
@@ -4,7 +4,6 @@ import Component from "../components/Component";
 const index = () => {
   return (
     <div>
-      <h1>Hello world!</h1>
       <Link href="/about">Go to about page</Link>
       <Component />
     </div>
