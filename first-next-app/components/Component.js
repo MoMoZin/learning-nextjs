594885a778ff632e13b65d04530a52b1a952736b
kmomozin <khinmomozin@gmail.com>
2023-04-11 11:18:04 -0600

    add react component to page

---
+++ first-next-app/components/Component.js
@@ -0,0 +1,7 @@
+const Component = () => {
+  return (
+    <h1>This is a Component</h1>
+  );
+};
+
+export default Component;
--- first-next-app/pages/index.js
+++ first-next-app/pages/index.js
@@ -1,10 +1,12 @@
 import Link from "next/link";
+import Component from "../components/Component";
 
 const index = () => {
   return (
     <div>
       <h1>Hello world!</h1>
       <Link href="/about">Go to about page</Link>
+      <Component />
     </div>
   );
 };
