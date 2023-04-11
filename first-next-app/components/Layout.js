e67dcc5d92026891902ba0f982af930f6b10a8de
kmomozin <khinmomozin@gmail.com>
2023-04-11 11:38:47 -0600

    add image using next image component

--- first-next-app/components/Layout.js
+++ first-next-app/components/Layout.js
@@ -1,8 +1,10 @@
 import Hello from "../components/Hello";
+import Image from "next/image";
 
 const Layout = ({ children }) => {
   return (
     <div className="container">
+      <Image src="https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_960_720.jpg" width={500} height={350} />
       <Hello />
       {children}
     </div>
